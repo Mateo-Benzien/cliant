2883fa13e9d7eb6fcbe5de7b5ef97224f0ed62d1
Mateo-Benzien <mateobenzien822@gmail.com>
2024-06-15 14:02:01 +0200

    updateing the sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import {useState} from "react";
 import styled, { ThemeProvider } from 'styled-components';
 import { darkTheme, lightTheme } from './utils/Themes.js'
+import Sidebar from './components/Sidebar'; // Correct import path
+
 const Container = styled.div`
+display: flex;
 background: ${({theme}) => theme.bg};
 width: 100%;
 height: 100vh;
+overflow-x: hidden;
+overflow-y: hidden;
 `;
 
 function App() {
@@ -12,7 +17,10 @@ function App() {
   const [darkMode, setDarkMode] =useState(true)
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <Container>Podstream</Container>
+      <Container>
+        <Sidebar /> 
+        Podstream
+        </Container>
     </ThemeProvider>
   
 )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,34 @@
-import React from `react`
+import React from "react";
+import styled from "styled-components";
+import {HomeRounded,CloseRounded} from "@mui/icons-material";
 
-const Sidebar = () =>{
-    return(
-        <div>Sidebar</div>
-    )
-}
-export default Sidebar
+const MenuContainer = styled.div`
+flex: 0.5;
+flex-direction: column;
+height: 100vh;
+display: flex;
+background-color: ${({theme}) => theme.bg};
+color: ${({theme}) => theme.text_primary};
+`;
+const Logo = styled.div ``;
+const Close = styled.div ``;
+const Elements = styled.div ``;
+const NavText = styled.div ``;
+
+
+const Sidebar = () => {
+    return <MenuContainer>
+        <Logo>Podstream</Logo>
+        <Close>
+            <CloseRounded />
+        </Close>
+        
+        <Elements>
+            <HomeRounded />
+            <NavText>Dashboard</NavText>
+        </Elements>
+    </MenuContainer>;
+   
+};
+
+export default Sidebar;
