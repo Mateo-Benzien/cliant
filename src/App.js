53cb2c0c944c812f67f9bc4594c00c49d274dac1
Mateo-Benzien <mateobenzien822@gmail.com>
2024-06-15 11:00:53 +0200

    removed robots, updated code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import {useState} from "react";
+import styled, { ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from './utils/Themes.js'
+const Container = styled.div`
+background: ${({theme}) => theme.bg};
+width: 100%;
+height: 100vh;
+`;
+
 function App() {
+  //Hooks
+  const [darkMode,setDarkMode] =useState(true)
   return (
-    <div>
-      Podstream
-    </div>
-  );
+    <ThemeProvider theme={darkTheme}>
+      <Container>Podstream</Container>
+    </ThemeProvider>
+  
+)
+  ;
 }
 
 export default App;
