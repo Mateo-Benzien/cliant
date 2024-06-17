26f3e9c416866ac11eeedaa08f1cc9a1d1afdacb
Mateo-Benzien <mateobenzien822@gmail.com>
2024-06-17 19:04:38 +0200

    Starting with the Cards

---
+++ src/components/PodcastCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+const PodcastCard =() => {
+    return <Card>
+        <Top>
+        <FavouriteIcon />
+            <CardImage />
+        </Top>
+    </Card>
+};
+
+export default PodcastCard;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,85 @@
 import React from "react";
+import styled from "styled-components";
+import {Link} from "react-router-dom"
+import PodcastCard from "../components/PodcastCard";
+
+const DashboardMain = styled.div`
+paddin: 20px 30px;
+padding-bottom: 200px;
+height: 100%;
+overflow-y: scroll;
+display: flex;
+flex-direction: column;
+gap: 20px;
+@media (max-width: 768px) {
+padding: 6px 10px;
+}
+`;
+
+const FilterContainer = styled.div`
+display: flex;
+flex-direction: column;
+background-color: ${({ theme }) => theme.bg};
+  border-radius: 10px;
+  padding: 20px 30px;
+`;
+
+const Topic = styled.div`
+color: ${({ theme }) => theme.text_primary};
+font-size: 24px;
+font-weight: 500;
+display: flex;
+justify-content: space-between;
+alighn-items: center;
+@media (max-width: 768px) {
+font-size: 18px;
+}
+`;
+
+const Span = styled.div`
+color: ${({ theme }) => theme.text_primary};
+font-size: 16px;
+font-weight: 400;
+@media (max-width: 768px) {
+font-size: 14px;
+}
+`;
+
+const Podcast = styled.div``;
 
 const Dashboard =() => {
-    return <div>
-        Dashboard
-    </div>
+    return <DashboardMain>
+        <FilterContainer>
+            <Topic>
+                Most Popular
+            <Link to={`/showpodcasts/mostpopular`} style={{textDecoration: "none"}}>
+            <Span>Show All</Span>
+            </Link>
+            </Topic>
+            <Podcast>
+
+                <PodcastCard />
+                <PodcastCard />
+                <PodcastCard />
+
+            </Podcast>
+        </FilterContainer>
+
+        <FilterContainer>
+            <Topic>
+                Comedy
+            <Link to={`/showpodcasts/comedy`} style={{textDecoration: "none"}}>
+            <Span>Show All</Span>
+            </Link>
+            </Topic>
+            <Podcast>
+
+            <PodcastCard />
+            <PodcastCard />
+
+            </Podcast>
+        </FilterContainer>
+    </DashboardMain>
 };
 
 export default Dashboard;
