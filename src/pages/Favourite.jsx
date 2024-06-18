1156ec13618379de2e3842c5ae0e2b08ec1db2a8
Mateo-Benzien <mateobenzien822@gmail.com>
2024-06-18 09:02:34 +0200

    Favourites still doens't work but on to search

--- src/pages/Favourite.jsx
+++ src/pages/Favourite.jsx
@@ -1,24 +1,47 @@
-import { Topic } from '@mui/icons-material';
 import React from 'react';
-import PodcastCard from '../components/PodcastCard';
+import PodcastCard from '../components/PodcastCard.jsx';
 import styled from 'styled-components';
 
-const Container = styled.div``;
-const Topic = styled.div``;
-const FavouriteContainer = styled.div``;
+const Container = styled.div`
+  padding: 20px 30px;
+  padding-bottom: 200px;
+  height: 100%;
+  overflow-y: scroll;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 
-const Favourite=() => {
-  return <Container>
-      <Topic>
-        Favourites
-      </Topic>
+const Header = styled.div`
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 24px;
+  font-weight: 540;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FavouriteContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 14px;
+  padding: 18px 6px;
+  @media (max-width: 550px) {
+    justify-content: center;
+  }
+`;
+
+const Favourite = () => {
+  return (
+    <Container>
+      <Header>Favourites</Header>
       <FavouriteContainer>
         <PodcastCard />
         <PodcastCard />
         <PodcastCard />
       </FavouriteContainer>
     </Container>
- };
-
+  );
+};
 
 export default Favourite;
